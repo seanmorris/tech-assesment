30d0692daf5310ec7166e400123591e4346ba262
Sean Morris <sean@red-october.virgo>
2018-06-21 07:49:47 -0400

    menu js.

---
+++ public/js/menu.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded', (event)=>{
+	let menuToggle = document.querySelector('[data-toggle="menu"]');
+	let menuOpen   = false;
+	menuToggle.addEventListener('click', (event)=>{
+		console.log(menuToggle.classList);
+		
+		let parent = menuToggle.parentNode;
+
+		while(parent)
+		{
+			console.log(parent);
+			if(parent.matches('[data-togglable="menu"]') || parent == window)
+			{
+				break;
+			}
+			parent = parent.parentNode;
+		}
+
+		if(menuOpen)
+		{
+			parent.classList.remove('open');
+			menuToggle.classList.remove('open');
+			menuOpen = false;
+			return;
+		}
+		menuOpen = true;
+		parent.classList.add('open');
+		menuToggle.classList.add('open');
+	});
+});
