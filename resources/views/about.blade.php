dfd6dd9f404344b57593b34952afff0258c06d3d
Sean Morris <sean@red-october.virgo>
2018-06-24 05:37:34 -0400

    About.

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,1 +1,7 @@
 <h2>About</h2>
+
+<p>Data provided by <a href = "http://www.retrosheet.org/">retrosheet.org</a></p>
+
+<p>Images by <a href = "https://picsum.photos/">picsum.photos</a></p>
+
+<p>Filler text provided by <a href = "https://www.lipsum.com/">lipsum.com</a>
