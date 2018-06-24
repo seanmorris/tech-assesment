4fbf05e0ad12fa1fda1be6c6a3b96586c54d042a
Sean Morris <sean@the-altar.virgo>
2018-06-24 17:00:27 -0400

    Non blocking CSS.

--- resources/views/chrome.blade.php
+++ resources/views/chrome.blade.php
@@ -15,9 +15,9 @@
             			<li><a href = "/home">Home</a></li>
             			<li><a href = "/news">News</a></li>
                         <li><a href = "/events">Events</a></li>
+                        <li><a href = "/tracking">Tracking</a></li>
                         <li><a href = "/about">About</a></li>
                         <li><a href = "/blank">Blank</a></li>
-                        <li><a href = "/tracking">Tracking</a></li>
             		</ul>
                     <div class = "topRight narrow">
                         <div class = "hamburger" data-toggle="menu">
